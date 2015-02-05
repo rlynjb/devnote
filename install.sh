317a79084053f4354f4cb7430bd576ddfeb390cc
rlynjb <rlynjb@gmail.com>
2015-02-05 11:53:51 -0800

    add indent on install function

--- install.sh
+++ install.sh
@@ -17,29 +17,29 @@ done
 
 # Check or install zsh
 install_zsh() {
-# test to see if zshell is installed. if it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-	# clone my oh-my-zsh repo from github only if it isnt already
-	if [[ ! -d $dir/oh-my-zsh/ ]]; then
-		git clone http://github.com/robbyrussell/oh-my-zsh.git
-	fi
-	# set the default shell to zsh if it isnt currently set to zsh
-	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-		chsh -s $(which zsh)
-	fi
-else
-	# If zsh isn't installed, get the platform of the current machine
-	platform=$(uname);
-	# If the platform is Linux, try an apt-get to install zsh and then recurse
-	if [[ $platform == 'Linux' ]]; then
-      		sudo apt-get install zsh
-        	install_zsh
-    	# If the platform is OS X, tell the user to install zsh :)
-    	elif [[ $platform == 'Darwin' ]]; then
-        	echo "Please install zsh, then re-run this script!"
-        	exit
-    	fi
-fi
+  # test to see if zshell is installed. if it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # clone my oh-my-zsh repo from github only if it isnt already
+    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+      git clone http://github.com/robbyrussell/oh-my-zsh.git
+    fi
+    # set the default shell to zsh if it isnt currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      chsh -s $(which zsh)
+    fi
+  else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+            sudo apt-get install zsh
+            install_zsh
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
+  fi
 }
 
 install_zsh
